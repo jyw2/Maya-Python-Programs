209c46bcafa650894e93bf00dd4ed5205dd4dc7f
jyw2 <jyw2@ualberta.ca>
2021-01-17 14:26:26 -0700

    Refactored and readme updated

--- chain_maker.py
+++ chain_maker.py
@@ -1,15 +1,14 @@
 from maya import cmds
 import math
 
-
-
 class Window:
-	"""Creates a window"""
+	"""Creates a window and an associated chain obj. Takes no arguments"""
+
 	def __init__(self):
 			
 		self.windowName = "Chain_Maker"
 
-		#if window already exists
+		#if window already exists close it first
 		if cmds.window(self.windowName, ex = True):
 			self.cancel()
 
@@ -45,42 +44,54 @@ class Window:
 		cmds.showWindow()
 
 	def mod_links(self,*args):
+		"""modifies the link in an existing chain. Note that the chain
+		is REPLACED with each call.Takes no arguments other than self"""
 		self.links = cmds.intSlider(self.linksSlider, q = True, v = True)
 		if self.links > 1:
-			cmds.delete(self.chain.delete_chain())
-			chain = Chain(self.links, radius= self.radius, linkRadius= self.radiusLink)
+			self.recreate_chain()
 			cmds.text(self.linkText,edit=True, label = str(self.links))
 
-	#Create new chain and modify thickness slider to prevent clipping
-	def mod_radius(self,*args):
 	
+	def mod_radius(self,*args):
+		"""Modifies the radius of an existing chain. Note that the chain
+		is REPLACED with each call.Takes no arguments other than self"""
+		#Create new chain and modify thickness slider to prevent clipping
 		self.radius = cmds.floatSlider(self.radiusSlider, q = True, v = True)
-		cmds.delete(self.chain.delete_chain())
-		print self.radius
-		chain = Chain(self.links, radius= self.radius, linkRadius= self.radiusLink)
+		self.recreate_chain()
 		cmds.floatSlider(self.thicknessSlider, e = True, max = self.radius/3)
 
-	#Create new chain and modify rad slider to prevent clipping
+	
 	def mod_thickness(self,*args):
+		"""modifies the thickness of an existing chain. Note that the chain
+		is REPLACED with each call.Takes no arguments other than self"""
+		#Create new chain and modify rad slider to prevent clipping
 		self.radiusLink = cmds.floatSlider(self.thicknessSlider, q = True, v = True)
 		print self.radiusLink
-		cmds.delete(self.chain.delete_chain())
-		chain = Chain(self.links, radius= self.radius, linkRadius= self.radiusLink)
+		self.recreate_chain()
 		cmds.floatSlider(self.radiusSlider, e = True, min= self.radiusLink*3)
 
+	def recreate_chain(self):
+		"""Deletes old chain and creates a new chain based on new parameters.Takes no arguments other than self"""
+		cmds.delete(self.chain.delete_chain())
+		chain = Chain(self.links, radius= self.radius, linkRadius= self.radiusLink)
+
 	def confirm(self,*args):
+		"""Closes the UI without affecting the chain.Takes no arguments other than self"""
 		cmds.deleteUI(self.windowName)
 
 
 
 	def cancel(self,*args):
+		"""Closes UI and deletes chain.Takes no arguments other than self"""
 		cmds.deleteUI(self.windowName)
 		cmds.delete(self.chain.delete_chain())
 
 
 
 class Ring():
-	"""A ring in the chain. Stores it's transform string,node string and place on the chain"""
+	"""A ring in the chain. Stores it's transform string,node string and place on the chain.
+	Arguments:
+		ringNumber: number of links or rings, radius: radius of the rings, linkRadius: radius of the thickness"""
 	def __init__(self,ringNumber,radius,linkRadius):
 
 		#diffirent naming for the first ring
@@ -88,20 +99,17 @@ class Ring():
 			(self.transform,self.shape) = cmds.polyTorus(r=radius,sr=linkRadius, name = "Chain_root")
 			self.ringName = self.transform
 
-			# change pivot to allow easier whole chain scaling
-			# cmds.setAttr(self.transform,ax)
-
-		#Naming for all other rings. Also parents the rings to the first ring.
+		#Naming for all other rings. 
 		else:
 			(self.transform,self.shape) = cmds.polyTorus(r=radius,sr=linkRadius, name = "ring_"+str(ringNumber))
 			self.ringName = self.transform
-			
-
 
 		self.ringNumber = ringNumber
 		
 	def add_parent(self,parentName):
+		"""updates the ring transform id after being parented"""
 		self.transform = parentName+"|"+self.transform	
+
 	def get_transform(self):
 		return self.transform
 	def get_shape(self):
@@ -112,6 +120,7 @@ class Ring():
 		return self.ringName
 
 	def delete_link(self):
+		"""Removes itself"""
 		cmds.delete(self.transform)
 
 		
@@ -156,18 +165,18 @@ class Chain():
 
 		#general transforms
 		ring = Ring(self.linkNumber,self.radius,self.linkRadius)
-		cmds.setAttr(ring.get_shape() + ".subdivisionsAxis", 4)
+		cmds.setAttr(ring.get_shape() + ".subdivisionsAxis", 4)# change subdivisions to get rectangular shape
 		cmds.setAttr(ring.get_transform() + ".rotateY",45) # rotation in xz plane
 		
 		cmds.polySelect(ring.get_transform(), el = 100) # stretching the square
 		cmds.polySelect(ring.get_transform(), el = 85,add = True)
 		cmds.polyMoveEdge(translateX = 0.8*(float(-self.radius)*(2.0-math.sqrt(2))/2))
 
-		cmds.polySelect(ring.get_transform(), el = 90)
+		cmds.polySelect(ring.get_transform(), el = 90)# stretching the square at opposite corners
 		cmds.polySelect(ring.get_transform(), el = 95, add = True)
 		cmds.polyMoveEdge(translateX =0.8*(float( self.radius)*(2.0-math.sqrt(2))/2))
 
-		#rotate
+		#rotate along chain axis every 2nd link
 		if self.rotationState == True:
 			cmds.rotate(90 ,0, 45, ring.get_transform(),ws=True)
 			
@@ -184,26 +193,22 @@ class Chain():
 		"""Creates and links in relationship all rings. The first
 		ring is the root(parent)"""
 
+		#Create links
 		for link in range(self.links):
 			self.create_link()
 
+		#parent all links to the first
 		for link in range(1,len(self.linkObjs)):
 			cmds.parent((self.linkObjs[link]).get_name(),self.linkObjs[0].get_name())
 			self.linkObjs[link].add_parent(self.linkObjs[0].get_transform())
 
+
 	def delete_chain(self):
+		"""Removes the chain"""
 		rootname = self.linkObjs[0].get_transform()
 		cmds.delete(rootname)
 
-		
-		
-
-			
-		
-
-
-#Tests
-#chain = Chain(20,radius=0.1, linkRadius=0.015)
+#initiates the program
 window = Window()
 
 
